255dbbd2ec7b7c41d0ceede8b4088e28030afcc2
garridopablo90 <35285352+garridopablo90@users.noreply.github.com>
2019-06-07 17:07:25 -0400

    Add files via upload

---
+++ 4_Habilidad_Perceptiva_2/ajax_habilidad_dos.js
@@ -0,0 +1,35 @@
+//console.log("correcto");
+
+//document.querySelector('#boton').addEventListener('click', traerDatos);
+
+function traerDatosDos () { 
+
+const xhttp = new XMLHttpRequest();
+
+xhttp.open('GET', 'catalogo_habilidad_dos.json', true);
+
+xhttp.send();
+
+xhttp.onreadystatechange = function(){
+
+    if(this.readyState == 4 && this.status == 200){
+        //console.log(this.responseText);
+        let datos2 = JSON.parse(this.responseText);
+        //console.log(datos);
+        let res2 = document.querySelector('#palabras2');
+        res2.innerHTML = '';
+
+        for(let item2 of datos2){
+            //console.log(item.titulo);
+            res2.innerHTML += `
+           
+              <p>${item2.palabra}</p>
+            
+            `
+        }
+    }
+
+ }
+
+
+}
---
+++ 5_Expansión_Ocular_2/ajax_expansion.js
@@ -0,0 +1,35 @@
+//console.log("correcto");
+
+//document.querySelector('#boton').addEventListener('click', traerDatos);
+
+function traerDatos () { 
+
+const xhttp = new XMLHttpRequest();
+
+xhttp.open('GET', 'catalogo_expansion.json', true);
+
+xhttp.send();
+
+xhttp.onreadystatechange = function(){
+
+    if(this.readyState == 4 && this.status == 200){
+        //console.log(this.responseText);
+        let datos = JSON.parse(this.responseText);
+        //console.log(datos);
+        let res = document.querySelector('#palabras2');
+        res.innerHTML = '';
+
+        for(let item of datos){
+            //console.log(item.titulo);
+            res.innerHTML += `
+           
+              <p>${item.palabra}</p>
+            
+            `
+        }
+    }
+
+ }
+
+
+}
